aec8f6b6efea69b4469cb5d6c3b4c57ab267500f
tapp41k <icepeak5@yandex.ru>
2023-12-23 22:33:55 +0300

    some fix

--- app/api/validators.py
+++ app/api/validators.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 
-from fastapi import HTTPException, status
+from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud.charity_project import charity_project_crud
@@ -63,17 +63,21 @@ async def update_full_amount_in_charity_project(
             status_code=HTTPStatus.BAD_REQUEST,
             detail='Закрытый проект нельзя редактировать!'
         )
+    project_in = project_in.dict(exclude_unset=True)
 
-    if 'full_amount' in project_in.dict(exclude_unset=True):
-        if charity_project.invested_amount > project_in.full_amount:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail='Параметр full_amount нельзя установить меньше внесённой суммы.'
-            )
-        if charity_project.full_amount < project_in.full_amount:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail='Параметр full_amount нельзя установить меньше текущего!'
-            )
+    if 'full_amount' not in project_in:
+        return charity_project
+
+    if charity_project.invested_amount > project_in['full_amount']:
+        raise HTTPException(
+            status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
+            detail='Параметр full_amount нельзя установить меньше внесённой cуммы.'
+        )
+
+    if charity_project.full_amount < project_in['full_amount']:
+        raise HTTPException(
+            status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
+            detail='Параметр full_amount нельзя установить меньше текущего!'
+        )
 
     return charity_project
